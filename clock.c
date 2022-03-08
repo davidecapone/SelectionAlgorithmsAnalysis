18b73ea0d3a96bc25b56be866a3131003f3032f2
davidecapone <davidecapone2304@gmail.com>
2022-03-08 17:52:06 +0100

    relazioni giornaliere

---
+++ clock.c
@@ -0,0 +1,25 @@
+/**
+ * @file clock.c
+ * @author Capone, Della Rovere, Gortani, Fior
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <time.h>
+
+double duration(struct timespec start, struct timespec end) {
+    return end.tv_sec - start.tv_sec
+         + ((end.tv_nsec - start.tv_nsec ) / (double) 1000000000.0);
+}
+double getResolution(){
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    do {
+        clock_gettime(CLOCK_MONOTONIC, &end);
+    } while (duration(start, end) == 0.0);
+    return duration(start, end);
+}
