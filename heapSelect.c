365917287d6e118ba19f4f7286b1d772e0ce4e8b
davidecapone <davidecapone2304@gmail.com>
2022-03-10 14:36:50 +0100

    heapSelect: swap, scanArray, heapify (finito)

--- heapSelect.c
+++ heapSelect.c
@@ -12,8 +12,75 @@
  * 
  */
 #include <stdio.h>
+#define MAX_LINE_SIZE 1000   // maximum size of a line of input
+
+int scanArray(int *A) {
+    // scan line of text
+    char line[MAX_LINE_SIZE];
+    scanf("%[^\n]", line);
+
+    // convert text into array
+    int size = 0, offset = 0, numFilled, n;
+    do {
+        numFilled = sscanf(line + offset, "%d%n", &(A[size]), &n);
+        if (numFilled > 0) {
+            size++;
+            offset += n;
+        }
+    } while (numFilled > 0);
+    
+    return size;
+}
+
+/**
+ * @brief scambia 2 elementi in A
+ * 
+ * @param A 
+ * @param i indice primo elemento
+ * @param j indice secondo elemento
+ */
+void swap(int A[], int i, int j){
+  
+  int key = A[i];
+  
+  A[i] = A[j];
+  A[j] = key;
+}
+
+/**
+ * @brief preservare le propietà della min-heap A
+ * 
+ * @param A min-heap 
+ * @param heapsize dimensione della heap (rimane invariata nella procedura)
+ * @param i posizione in è presente l'errore
+ */
+void heapify( int A[], int heapsize, int i ) {
+  int left = left( i );
+  int right = right( i );
+  int max;
+
+  if (left < heapsize && A[ left ] > A[ max ]) max = left;
+  if (right < heapsize && A[ right ] > A[ max ]) max = right;
+
+  if (max != i) {
+    swap(A, i, max);
+    heapify(A, heapsize, max);
+  }
+}
+
+/**
+ * @brief costruire una min-heap a partire da un vettore A
+ * 
+ * @param A vettore non min-heap
+ * @param dim dimensione della heap
+ */
+void buildMinHeap( int A[], int dim ) {}
 
 int main () {
+  // scan sullo standard input per definire il vettore
+  int A[MAX_LINE_SIZE];
+  int *p = &A[0];
+  int l = scanArray(p);
 
   return 0;
 }
