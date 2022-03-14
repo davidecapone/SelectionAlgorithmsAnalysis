104c69078763a8d7e608f911bcfc0faa4248b753
davidecapone <davidecapone2304@gmail.com>
2022-03-14 11:12:30 +0100

    calcolo tempo minimo misurabile

--- clock.c
+++ clock.c
@@ -8,13 +8,14 @@
  * @copyright Copyright (c) 2021
  * 
  */
-
+#include <stdio.h>
 #include <time.h>
 
 double duration(struct timespec start, struct timespec end) {
     return end.tv_sec - start.tv_sec
          + ((end.tv_nsec - start.tv_nsec ) / (double) 1000000000.0);
 }
+
 double getResolution(){
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
@@ -22,4 +23,18 @@ double getResolution(){
         clock_gettime(CLOCK_MONOTONIC, &end);
     } while (duration(start, end) == 0.0);
     return duration(start, end);
+}
+
+int main () {
+    // risoluzione stimata
+    double R = getResolution();
+
+    // valore relativo massimo ammissibile
+    double E = 0.001;
+
+    // tempo minimo misurabile
+    double Tmin = R * ( 1/E + 1 );
+
+    printf("%f\n", Tmin);
+    return 0;
 }
