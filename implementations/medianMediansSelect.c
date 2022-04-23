d0630ec2567612678f908abea14bc5cc35c5b308
Iury Fior <iuryfior@gmail.com>
2022-04-23 15:22:46 +0200

    MoM Place funziona su elearning
    il codice include le funzioni main, swap e scanArray. Queste devono essere rimosse quando MoM viene chiamato in Analysis.
    NB: il codice restituisce LA POSIZIONE DI K, NON IL VALORE! Infatti nel main come valore di output c'è arr[k] e NON il valore restituito da MoMSelect

--- implementations/medianMediansSelect.c
+++ implementations/medianMediansSelect.c
@@ -5,8 +5,7 @@
 #include <math.h> 
 #include <limits.h>
 #include <stdlib.h>
-#include "scanArray.h"
-#include "swap.h"
+#include <stdio.h>
 
 #define MAX_LINE_SIZE 5000   // maximum size of a line of input
 
@@ -16,6 +15,39 @@ int MoMPlace(int arr[], int p, int q);
 int MoMPartition(int arr[], int p, int q, int k, int posPerno);
 int med(int arr[], int p, int q);
 void insertionSort(int arr[], int p, int q );
+
+/**
+ * @brief scambia 2 elementi in A
+ * 
+ * @param A 
+ * @param i indice primo elemento
+ * @param j indice secondo elemento
+ */
+void swap( int A[], int i, int j ){
+  
+  int key = A[ i ];
+  
+  A[ i ] = A[ j ];
+  A[ j ] = key;
+}
+
+int scanArray(int *A) {
+    // scan line of text
+    char line[MAX_LINE_SIZE];
+    scanf("%[^\n]", line);
+
+    // convert text into array
+    int size = 0, offset = 0, numFilled, n;
+    do {
+        numFilled = sscanf(line + offset, "%d%n", &(A[size]), &n);
+        if (numFilled > 0) {
+            size++;
+            offset += n;
+        }
+    } while (numFilled > 0);
+    
+    return size;
+}
 
 /** 
  * @brief: trova l'indice di k in arr se arr fosse ordinato  
@@ -49,7 +81,7 @@ int MoMSelect(int arr[], int p, int q, int k){
   */ 
  
   if(k == posPerno){ 
-    return arr[k]; 
+    return k; 
   } else if(k < posPerno){ 
     q = posPerno - 1; 
     return MoMSelect(arr, p, q, k); 
@@ -206,4 +238,16 @@ int med(int arr[], int p, int q){
  
   insertionSort(arr, p, q); 
   return ceil( (p+q)/2.0 ); 
+}
+
+int main(){
+    
+    int arr[ MAX_LINE_SIZE ] = {};
+    int size = scanArray( arr );
+    int k;
+    scanf("%d", &k);
+    int kSmallest = MoMSelect(arr, 0, size-1, k-1);  
+    printf("%d", arr[kSmallest]);
+    
+    return 0;
 }
